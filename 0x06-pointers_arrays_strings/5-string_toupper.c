9478d2b26edd5e5a6ceed189e1d6294f83e0de5c
Emeamos <emediongamos.engr@gmail.com>
2022-09-22 20:22:43 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,17 +8,16 @@
 char *string_toupper(char *x)
 {
 	int length;
-
+i
 	length = 0;
 
 	while (x[length] != '\0')
 	{
 		if (x[length] >= 97 && x[length] <= 122)
 		{
-			x[length] = x[length] - 32
+			x[length] = x[length] - 32;
 		}
 		length++;
 	}
-
 	return (x);
 }
