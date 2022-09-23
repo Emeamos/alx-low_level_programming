64df2f7f2d60df82b9fd71cfe8ce1022c603e6ef
Emeamos <emediongamos.engr@gmail.com>
2022-09-22 20:26:48 -0700

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * reverse_array - revereses an array.
- * @a: pointer to array.
- * @n: number of elements of an array.
+ *reverse_array - revereses an array.
+ *@a: pointer to array.
+ *@n: number of elements of an array.
  *
- *  *Return: void.
+ *Return: void.
  */
 void reverse_array(int *a, int n
 {
